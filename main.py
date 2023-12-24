b978ba076bbc489c69e3a83e573c4829f163ec72
Navid Shirmohammadi <mailt9681@gmail.com>
2023-12-24 16:59:45 +0330

    working

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ proxies = {
 
 username = os.environ.get("UI_USERNAME")
 password = os.environ.get("UI_PASSWORD")
+
+if not username:
+   print("please set UI_USERNAME environment variable")
+   exit()
+
+if not password:
+   print("please set UI_PASSWORD environment variable")
+   exit()
+
 url = "http://logout.ui.ac.ir"
 extraction_pattern = re.compile("\S*document.sendin.password.value = hexMD5\((.*)\);")
 replacement_pattern = re.compile("' \+ document.login.password.value \+ '")
@@ -24,10 +33,12 @@ scripts = soup.find_all('script')
 for script in scripts:
    search = extraction_pattern.search(str(script.string))
    if search:
+      print(f"search: {search.group(1)}")
       pre_hash = replacement_pattern.sub(password, search.group(1))
       hash_str = ""
 
       with open('./md5.js', 'r') as f:
+        print("found pattern")
         content = f.read()
         content += f'var result = hexMD5({pre_hash})'
         ctx = execjs.compile(content)
@@ -37,8 +48,30 @@ for script in scripts:
         "username": username,
         "password": hash_str,
         "dst": "",
-        "popup": False,
+        "popup": "true",
       }
 
-      response =  requests.post(url, json=data)      
+      headers = {
+        "Cache-Control": "max-age=0",
+        "Upgrade-Insecure-Requests": "1",
+        "Origin": "http://logout.ui.ac.ir",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.110 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Referer": "http://logout.ui.ac.ir/login",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "en-US,en;q=0.9",
+      }
+
+      # send login request
+      response =  requests.post(
+         f"{url}/login", 
+         data=data, 
+         headers=headers,
+         proxies=proxies)
+
+      # check status page
+      response =  requests.get(
+         f"{url}/status",
+         headers=headers,
+         proxies=proxies)
       print(response.text)
